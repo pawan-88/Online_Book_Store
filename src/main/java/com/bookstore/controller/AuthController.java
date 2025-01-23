92edb2c2d7c906ad8b93c99a295315c636e6c7a6
pawan-88 <pawansanap8@gmail.com>
2025-01-23 15:07:55 +0530

    updated code for warehouse rack and postman format

--- src/main/java/com/bookstore/controller/AuthController.java
+++ src/main/java/com/bookstore/controller/AuthController.java
@@ -11,10 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
